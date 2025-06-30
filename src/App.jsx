5847a005aeb73ccefebfc809266fa0df91615532
Grooveflow <grooveflowdance@gmail.com>
2025-07-01 04:58:41 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Studio from './pages/Studio'
-import Subscribe from './pages/Subscribe'
-
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/studio" element={<Studio />} />
-      <Route path="/subscribe" element={<Subscribe />} />
-    </Routes>
-  )
+    <div>
+      <h1>Welcome to Dance2This!</h1>
+      <p>Your React app is working. 🎉</p>
+    </div>
+  );
 }
-export default function App() {
-    return (
-        <div>
-              <h1>Welcome to Dance2This!</h1>
-                    <p>This is your React app up and running. 🎉</p>
-                        </div>
-                          );
-                          }

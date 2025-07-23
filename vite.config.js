67f51173fb554a140b89121d34962a069d1ba133
Grooveflow <grooveflowdance@gmail.com>
2025-07-23 10:41:26 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,3 @@ export default defineConfig({
   }
 });
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-});
